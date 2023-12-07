652a027eb708f2ce8612f38f71205b9abe89404e
Dutraz <53490994+Dutraz@users.noreply.github.com>
2023-12-07 19:08:19 -0300

    adding the meshs

--- main.js
+++ main.js
@@ -10,38 +10,8 @@ import {TextGeometry} from 'three/addons/geometries/TextGeometry.js';
  */
 const canvas = document.querySelector('canvas.webgl');
 const scene = new THREE.Scene();
-
-
-/**
- * MESH's
- */
-const fbxLoader = new FBXLoader();
-fbxLoader.load(
-    '/models/office-cubicle/source/office-cubicle.fbx',
-    (obg) => {
-        obg.scale.set(0.0012, 0.0012, 0.0012);
-        obg.position.set(1.0, -0.5, 0);
-        obg.rotation.set(0.2, 2.0, 0);
-        scene.add(obg);
-    }
-);
-
-const fontLoader = new FontLoader();
-fontLoader.load(
-    'fonts/kdam_thmor_pro_regular.json',
-    (font) => {
-        const textGeometry = new TextGeometry('Pedro Dutra', {
-            font: font,
-            size: 0.9,
-            height: 0.000001,
-            curveSegments: 20,
-        });
-        const textMaterial = new THREE.MeshBasicMaterial({color: 0xffffff});
-        const textMesh = new THREE.Mesh(textGeometry, textMaterial);
-        textMesh.position.set(-7, 0.5, -3);
-        scene.add(textMesh);
-    }
-);
+let mixer = null;
+let action = null;
 
 
 /**
@@ -49,6 +19,60 @@ fontLoader.load(
  */
 const ambientLight = new THREE.AmbientLight(0xFFFFFF);
 scene.add(ambientLight);
+
+const sun = new THREE.DirectionalLight(0xFFFFFF, 0.5);
+sun.radius = 10;
+sun.castShadow = true;
+sun.position.set(0, 0, 1);
+scene.add(sun);
+
+
+/**
+ * MESH's
+ */
+const fbxLoader = new FBXLoader();
+fbxLoader.load(
+    '/models/office-cubicle/source/cenario.fbx',
+    (obj, materials) => {
+        obj.scale.setScalar(0.2);
+        obj.position.set(250, -140, 0);
+        obj.rotation.set(0.4, -0.75, 0);
+
+        obj.traverse((child) => {
+            if (child.isMesh) {
+                child.material.transparent = false;
+                child.castShadow = true;
+                child.receiveShadow = true;
+            }
+        });
+
+        mixer = new THREE.AnimationMixer(obj);
+        action = mixer.clipAction(obj.animations[2]);
+        action.play();
+
+        sun.target = obj;
+        scene.add(obj);
+    },
+);
+
+
+const fontLoader = new FontLoader();
+fontLoader.load(
+    'fonts/kdam_thmor_pro_regular.json',
+    (font) => {
+        const textGeometry = new TextGeometry('Pedro Dutra', {
+            font: font,
+            size: 75,
+            height: 1,
+            curveSegments: 20,
+        });
+        const textMesh = new THREE.Mesh(textGeometry, textMaterial);
+        textMesh.position.set(-550, -30, -10);
+        textMesh.castShadow = true;
+        textMesh.receiveShadow = true;
+        scene.add(textMesh);
+    }
+);
 
 
 /**
@@ -59,26 +83,18 @@ const sizes = {
     height: window.innerHeight
 };
 
-window.addEventListener('resize', () => {
-    // Update sizes
-    sizes.width = window.innerWidth;
-    sizes.height = window.innerHeight;
-
-    // Update camera
-    camera.aspect = sizes.width / sizes.height;
-    camera.updateProjectionMatrix();
-
-    // Update renderer
-    renderer.setSize(sizes.width, sizes.height);
-    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
-});
-
 
 /**
  * CAMERA
  */
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
-camera.position.z = 3;
+const camera = new THREE.OrthographicCamera(
+    sizes.width / -2,
+    sizes.width / 2,
+    sizes.height / 2,
+    sizes.height / -2,
+    -1000,
+    1000
+);
 scene.add(camera);
 
 const renderer = new THREE.WebGLRenderer({
@@ -86,6 +102,7 @@ const renderer = new THREE.WebGLRenderer({
     alpha: true
 });
 renderer.setSize(sizes.width, sizes.height);
+renderer.shadowMap.enabled = true;
 
 
 /**
@@ -98,6 +115,12 @@ const tick = () => {
     const deltaTime = elapsedTime - previousTime;
     previousTime = elapsedTime;
 
+    if (mixer) {
+        mixer.setTime(elapsedTime);
+    }
+
+    sun.position.x = Math.cos(elapsedTime * 0.9) * sizes.width / 1.2;
+    sun.position.y = Math.sin(elapsedTime * 0.9) * sizes.height / 1.2;
 
     renderer.render(scene, camera);
     window.requestAnimationFrame(tick);
